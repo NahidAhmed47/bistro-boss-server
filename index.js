db74749943d63dae4de1742441e371e95e03d1c4
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-05-24 17:54:29 +0600

    data get from mongodb collection

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const menuCollection = client.db('bistro-boss-db').collection('menu');
+    const reviewsCollection = client.db('bistro-boss-db').collection('reviews');
+    app.get('/menu', async (req, res) => {
+        const result = await menuCollection.find().toArray();
+        res.send(result);
+    })
+    app.get('/reviews', async (req, res) => {
+        const result = await reviewsCollection.find().toArray();
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
